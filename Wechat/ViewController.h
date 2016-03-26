6c97bafef202e77b16620c13c2c7d3a79d9cefc1
guang333xu <guang333xu@163.com>
2016-03-26 16:49:44 +0800

    Initial Commit

---
+++ Wechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wechat
+//
+//  Created by wang on 16/3/26.
+//  Copyright © 2016年 王光旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
